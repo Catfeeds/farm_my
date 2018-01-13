2c388d8925b3b7529e50fc319c9569919779c707
罗恒一 <1404814213@qq.com>
2018-01-13 18:08:16 +0800

    完善发放接口

--- Application/Admin/Controller/ProvideApi.php
+++ Application/Admin/Controller/ProvideApi.php
@@ -13,10 +13,63 @@ namespace Admin\Controller;
  * Class ProvideApi
  * @package Admin\Controller
  */
-const token = 'nsjkfhsdjkhfu';
+use Admin\Model\Bonus;
+use Home\Model\UserpropertyModel;
+use Think\Exception;
+
+const TOKEN = 'nsjkfhsdjkhfu';
+
+const COUNT = 1000;
 
 class ProvideApi{
 
+    #红包的发放
+    public function bonus(){
 
+        $bonus_m  =  new Bonus();
+
+        $where = [];
+
+        $count = $bonus_m->getCount($where);
+
+        $bonus_m->startTrans();
+        try{
+
+            for ($i = 0; $i<$count/C('Count');$i++){
+
+                $data = $bonus_m->getDataPage($where);
+
+                foreach ($data as $k=>$v){
+
+                    #返回金额
+
+                    #判断是否出局
+
+                    #返回用户金额
+                    $userproperty = new UserpropertyModel();  //home 模块的 Userproperty
+
+                    $back = $userproperty->setChangeMoney(1,);
+
+                    if (!$back){
+                        throw new Exception($userproperty->getError());
+                    }
+
+                }
+
+
+            }
+
+            $bonus_m->commit();
+
+        }catch (\Exception $e){
+
+            $this->error($e->getMessage());
+
+            $bonus_m->rollback();
+        }
+
+
+
+    }
 
 }
--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -16,5 +16,5 @@ define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', 'x"f*EoceK_AhJGvOYjmiy~;CkZg^0!bD9P7)u@Lr'); //加密KEY
 //user:vhcHusRbWHe9rSGH@192.168.0.112:3306
-define('UC_DB_DSN', 'mysql://root:root@127.0.0.1:3306/farm'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysql://root:root@127.0.0.1:3306/farm');  //数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'currency_'); // 数据表前缀，使用Model方式调用API必须配置此项
