d85d7a40df2666a1e0defb6855f7ca4c3e11614b
罗恒一 <1404814213@qq.com>
2018-01-15 11:41:14 +0800

    红包发放

--- Application/Admin/Controller/ProvideApi.php
+++ Application/Admin/Controller/ProvideApi.php
@@ -14,7 +14,11 @@ namespace Admin\Controller;
  * @package Admin\Controller
  */
 use Admin\Model\Bonus;
+use Admin\Model\BonusAllModel;
+use Admin\Model\BonusModel;
+use Admin\Model\RepeatCfgModel;
 use Home\Model\UserpropertyModel;
+use function Sodium\add;
 use Think\Exception;
 
 const TOKEN = 'nsjkfhsdjkhfu';
@@ -26,14 +30,28 @@ class ProvideApi{
     #红包的发放
     public function bonus(){
 
-        $bonus_m  =  new Bonus();
+        $bonus_m  =  new BonusModel();
 
-        $where = [];
+        $repeatCfgModel = new RepeatCfgModel();
+
+        #日返率
+        $date_back = $repeatCfgModel->getCfg('date_back');
+
+        #本次总发放数
+        $all_money = 0;
+
+        $where = [''=>''];
 
         $count = $bonus_m->getCount($where);
 
         $bonus_m->startTrans();
         try{
+
+            $bonusAllModel =  new BonusAllModel();
+            $nullBonusAll = $bonusAllModel->addNullBonusAll();
+            if (!$nullBonusAll){
+                throw new Exception('发放失败！');
+            }
 
             for ($i = 0; $i<$count/C('Count');$i++){
 
@@ -41,21 +59,43 @@ class ProvideApi{
 
                 foreach ($data as $k=>$v){
 
-                    #返回金额
+                    #应返金额(购买数量*日返金额)
+                    $money = $v['number']*(int)$date_back;
 
-                    #判断是否出局
+                    #判断出局金额与应返金额的关系
+
+                    $money = $money >= $v['out'] ? $money : $v['out'];
+
+                    $all_money += $money;
 
                     #返回用户金额
                     $userproperty = new UserpropertyModel();  //home 模块的 Userproperty
 
-                    $back = $userproperty->setChangeMoney(1,);
+                    #发放用户金额
+                    $back = $userproperty->setChangeMoney(1,$money,$v['user_id'],'红包分红',2);
 
                     if (!$back){
                         throw new Exception($userproperty->getError());
                     }
 
+                    #生成发放流水
+                    $back = $bonus_m->saveData($v['id'],$money);
+                    if (!$back){
+                        throw new Exception($userproperty->getError());
+                    }
+
+
                 }
 
+
+            }
+
+            #成功后修改本次发放总数
+            $back = $nullBonusAll->saveNullBonusAll($all_money);
+
+            if (!$back){
+
+                throw new Exception('发放失败！');
 
             }
 
@@ -67,9 +107,7 @@ class ProvideApi{
 
             $bonus_m->rollback();
         }
-
-
-
+        
     }
 
 }
